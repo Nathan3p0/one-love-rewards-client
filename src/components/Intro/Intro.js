3df79592588b6fe8b0f6e79e050901ed386a4a54
Nathan Szelag <nszelag@live.com>
2019-09-20 00:48:05 +0000

    added button styling and colors

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -8,8 +8,8 @@ const Intro = (props) => {
             <div className="main__content-intro--container">
                 <img src={largeLogo} alt="" className="main__content-intro--logo" />
                 <h2>REWARDS MADE EASY</h2>
-                <button onClick={props.handleCustomerLoginToggle} className="customer__btn">Customer Login</button>
-                <button onClick={props.handleBusinessLoginToggle} className="business__btn">Business Login</button>
+                <button onClick={props.handleCustomerLoginToggle} className="btn customer__btn">Customer Login</button>
+                <button onClick={props.handleBusinessLoginToggle} className="btn business__btn">Business Login</button>
             </div>
         </section>
     )
